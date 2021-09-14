c75b8f600160758f1d1790ab122687b2e8c58537
dadiakov <74093790+dadiakov@users.noreply.github.com>
2021-09-14 11:22:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,37 @@ const public = path.join(__dirname,'/public');
 
 const app = new Koa();
 
-app.use(cors());
+app.use(async (ctx, next) => {
+  const origin = ctx.request.get('Origin');
+  if (!origin) {
+    return await next();
+  }
+
+  const headers = { 'Access-Control-Allow-Origin': '*', };
+
+  if (ctx.request.method !== 'OPTIONS') {
+    ctx.response.set({ ...headers });
+    try {
+      return await next();
+    } catch (e) {
+      e.headers = { ...e.headers, ...headers };
+      throw e;
+    }
+  }
+
+  if (ctx.request.get('Access-Control-Request-Method')) {
+    ctx.response.set({
+      ...headers,
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
+    });
+
+    if (ctx.request.get('Access-Control-Request-Headers')) {
+      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
+    }
+
+    ctx.response.status = 204;
+  }
+});
 
 app.use(koaBody({
   urlencoded: true,
